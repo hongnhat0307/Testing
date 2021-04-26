fc3bc24db96d50f94cfe80c6ab0c726a7999f107
Nhat <hongnhat0307@gmail.com>
2021-04-26 23:31:18 +1000

    conflict?

--- just testing.R
+++ just testing.R
@@ -1,7 +1,9 @@
-print "This file was created within RStudio"
+print ("This file was created within RStudio")
 
-print "what is happening to the x?"
+print ("what is happening to the x?")
 
-print "what will it do then?"
-print "this is annoiying"
+print ("what will it do then?")
+print ("this is annoiying")
+
+print ("I think I have been able to solve it")
 
